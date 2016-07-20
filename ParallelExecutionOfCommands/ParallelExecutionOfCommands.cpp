daaa5328fe7ce6edfdbd32b2a740a5a00aaab176
nmitev <nmitev@propeople.dk>
2016-07-20 14:12:46 +0300

    Creting UI for the application.

--- ParallelExecutionOfCommands/ParallelExecutionOfCommands.cpp
+++ ParallelExecutionOfCommands/ParallelExecutionOfCommands.cpp
@@ -12,17 +12,39 @@
 
 using namespace std;
 
+int menu()
+{
+	// Creting application menu so the user can chose between parallel and successively execution.
+	int option;
+	cout << "Select one of the following options:" << endl;
+	cout << "1 - Parallel execution." << endl;
+	cout << "2 - Successively execution." << endl;
+	cout << "Selection: ";
+	cin >> option;
+	if (option<1 || option>2)
+	{
+		cout << "You have entered incorect option." << endl;
+		menu();
+	}
+	return option;
+}
+
 int main()
 {	
+
 	// Declaration of needed variables.
 	int processes_Count;
+	int menu_Option;
 	char command[128];
 	string behat_Profile;
 	string behat_Feature_Folder;
 	string behat_Bin = "bin/behat -p ";
 	string result_Command_String;
 	thread thread_Array[100];
-		
+	
+	//Getting users prefert execution type.
+	menu_Option = menu();
+
 	// Entering values for the variables.
 	cout << "Enter number of processes/threads you want to execute: ";
 	cin >> processes_Count;
@@ -38,15 +60,32 @@ int main()
 	cout << "You will execute " << processes_Count << " processes/threads with the following command: " << command << ".\n";
 
 	// Creating threads to handle the command execution.
-	for (size_t i = 0; i < processes_Count; i++)
+	if (menu_Option = 1)
 	{
-		thread_Array[i] = thread(system, command);
+		// Parallel execution mode.
+		for (size_t i = 0; i < processes_Count; i++)
+		{
+			thread_Array[i] = thread(system, command);
+		}
+		for (size_t i = 0; i < sizeof(thread_Array); i++)
+		{
+			thread_Array[i].join();
+			thread_Array[i].detach();
+		}
 	}
-	for (size_t i = 0; i < sizeof(thread_Array); i++)
-	{
-		thread_Array[i].join();
-		thread_Array[i].detach();
+	else
+	{	// Successively execution mode.
+		for (size_t i = 0; i < processes_Count; i++)
+		{
+			thread_Array[i] = thread(system, command);
+			thread_Array[i].join();
+		}
+		for (size_t i = 0; i < sizeof(thread_Array); i++)
+		{
+			thread_Array[i].detach();
+		}
 	}
+	
 
 	//// Creating process for execution of the command.
 	//pid_t pids[processes_Count];
